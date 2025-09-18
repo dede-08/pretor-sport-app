58836162086126393b9a0e9a5ad5e13aea697842
dede-08 <benandrealanyaguevara@gmail.com>
2025-09-18 18:40:38 -0500

    update

--- com.pretor-sport.app/com.pretor-sport.app/src/main/java/com/pretor_sport/app/PretorSportBackendApplication.java
+++ com.pretor-sport.app/com.pretor-sport.app/src/main/java/com/pretor_sport/app/PretorSportBackendApplication.java
@@ -15,22 +15,5 @@ public class PretorSportBackendApplication {
         SpringApplication.run(PretorSportBackendApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner createAdminUser(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
-//		return args -> {
-//			String adminEmail = "admin@pretor.com";
-//			if (usuarioRepository.findByEmail(adminEmail).isEmpty()) {
-//				Usuario admin = new Usuario();
-//				admin.setNombre("Admin");
-//				admin.setApellidos("Pretor Sport");
-//				admin.setEmail(adminEmail);
-//				admin.setPassword(passwordEncoder.encode("pretorsportadmin100@"));
-//				admin.setRol(Usuario.Rol.ROLE_ADMIN);
-//				admin.setActivo(true);
-//				admin.setEmailVerificado(true);
-//				usuarioRepository.save(admin);
-//				System.out.println("Usuario administrador creado: " + adminEmail);
-//			}
-//		};
-//	}
+
 }
